024f2350b6c67ff14e381950e339d52f19d9b7e9
Pablo Martín Escudero <escuderopablo@hotmail.com.ar>
2021-10-12 21:02:17 -0300

    Cambios que realmente no recuerdo :)

--- server/server.js
+++ server/server.js
@@ -1,37 +1,39 @@
-var express = require('express');
-var app = express();
-var appRouter = require('./helpers/appRouter.js').appRouter;
-var _a = require('./helpers/socketFunctions.js'), manageNewProduct = _a.manageNewProduct, manageNewMessage = _a.manageNewMessage, persistentHistory = _a.persistentHistory;
-var handlebarsEngine = require('./helpers/handlebars');
-var PORT = process.env.PORT || 8080;
+const express = require('express');
+const app = express();
+const {appRouter, productos} = require('./helpers/appRouter.js');
+const handlebarsEngine = require('./helpers/handlebars');
+const product = require('./helpers/newProduct');
+
+const PORT = process.env.PORT || 8080;
 app.use('/api', appRouter);
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 // app.use('/',(req, res, next)=>{
 //     console.log(req.url)
 //     next()
 // })
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-http.listen(PORT, function () {
-    console.log("Server initializated on port " + PORT);
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+
+http.listen(PORT, ()=>{
+    console.log(`Server initializated on port ${PORT}`)
 });
-io.on('connection', function (socket) {
-    console.log('User connected');
-    socket.emit('message', 'mensaje socket');
-    persistentHistory(socket);
-    socket.on('newProduct', function (data) {
-        manageNewProduct(data, socket);
-    });
-    socket.on('newMessage', function (msg) {
-        manageNewMessage(msg, socket, io);
-    });
-});
-//function to export socket.io to other js files.
-function getSocketFromApp() {
-    return io;
-}
-//function to modularize handlebars config.
+
+io.on('connection', (socket)=>{
+    console.log('User connected')
+    socket.emit('message', 'mensaje socket')
+    socket.on('newProduct',(data)=>{
+        const{title, price, thumbnail}=data;
+        newProd = new product(title, price, thumbnail);
+        productos.length < 1 ? newProd.productId(0) : newProd.productId(productos.length)
+        productos.push(newProd)
+        socket.emit('sentProduct', {newProd, productos});
+    })
+})
+
+
 handlebarsEngine(app);
-module.exports.importedIo = getSocketFromApp;
+
